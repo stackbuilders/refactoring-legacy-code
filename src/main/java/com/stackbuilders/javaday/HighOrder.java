5d9de53cda359564ed4a1be62b3b49a1c180f591
Sebastián Estrella <sestrella.me@gmail.com>
2019-07-09 00:40:46 -0500

    Test filter

--- src/main/java/com/stackbuilders/javaday/HighOrder.java
+++ src/main/java/com/stackbuilders/javaday/HighOrder.java
@@ -7,18 +7,7 @@ import java.util.List;
 // - First class citizens
 // - High order functions
 public class HighOrder {
-
-  // Explain <A, B>
-  // explain high order function
-  public static <A, B> List<B> map(F<A, B> f, A[] xs) {
-    List<B> ys = new ArrayList<B>();
-    for (A x : xs) {
-      ys.add(f.apply(x));
-    }
-    return ys;
-  }
-
-  public <A> List<A> filter(F<A, Boolean> f, A[] xs) {
+  public static <A> List<A> filter(F<A, Boolean> f, A[] xs) {
     List<A> ys = new ArrayList<A>();
     for (A x : xs) {
       if (f.apply(x)) {
@@ -28,7 +17,9 @@ public class HighOrder {
     return ys;
   }
 
-  public <A, B> B fold(F<A, F<B, B>> f, A[] xs, B initialValue) {
+  // Too verbose... Explain currying?
+  // Explain why final is required
+  public static <A, B> B fold(F<A, F<B, B>> f, A[] xs, B initialValue) {
     B acc = initialValue;
     for (A x : xs) {
       acc = f.apply(x).apply(acc);
@@ -36,37 +27,13 @@ public class HighOrder {
     return acc;
   }
 
-  // Java 1.6
-  public void mapUsage() {
-    Integer[] numbers =  new Integer[] { 1, 2, 3, 4, 5, 6 };
-    // Functions that take other functions as parameters
-    map(new F<Integer, Integer>() {
-      public Integer apply(Integer number) {
-        return number + 2;
-      }
-    }, numbers);
-
-    // Assign a function to a variable
-    F<Integer, Integer> addOne = new F<Integer, Integer>() {
-      public Integer apply(Integer number) {
-        return number + 1;
-      }
-    };
-    map(addOne, numbers);
-  }
-
-  public void foldUsage() {
-    Integer[] numbers =  new Integer[] { 1, 2, 3, 4, 5, 6 };
-    fold(new F<Integer, F<Integer, Integer>>() {
-      // Too verbose... Explain currying?
-      // Explain why final is required
-      public F<Integer, Integer>apply(final Integer x) {
-        return new F<Integer, Integer>() {
-          public Integer apply(Integer acc) {
-            return x + acc;
-          }
-        };
-      }
-    }, numbers, 0);
+  // Explain <A, B>
+  // explain high order function
+  public static <A, B> List<B> map(F<A, B> f, A[] xs) {
+    List<B> ys = new ArrayList<B>();
+    for (A x : xs) {
+      ys.add(f.apply(x));
+    }
+    return ys;
   }
 }
--- src/test/java/com/stackbuilders/javaday/HighOrderTest.java
+++ src/test/java/com/stackbuilders/javaday/HighOrderTest.java
@@ -8,14 +8,25 @@ import org.junit.Test;
 
 public class HighOrderTest {
   @Test
-  public void mapNonEmptyListAppliesFunctionToEachElement() {
-    Integer[] numbers = new Integer[] { 1, 2, 3, 4 };
-    F<Integer, Integer> addOne = new F<Integer, Integer>() {
-      public Integer apply(Integer x) {
-        return x + 1;
+  public void filterEmptyListReturnsEmptyList() {
+    Integer[] numbers = new Integer[] {};
+    F<Integer, Boolean> isEven = new F<Integer, Boolean>() {
+      public Boolean apply(Integer x) {
+        return x % 2 == 0;
       }
     };
-    assertThat(map(addOne, numbers), contains(2, 3, 4, 5));
+    assertThat(filter(isEven, numbers), empty());
+  }
+
+  @Test
+  public void filterNonEmptyListReturnsElementsMatchingPredicate() {
+    Integer[] numbers = new Integer[] { 1, 2, 3, 4 };
+    F<Integer, Boolean> isEven = new F<Integer, Boolean>() {
+      public Boolean apply(Integer x) {
+        return x % 2 == 0;
+      }
+    };
+    assertThat(filter(isEven, numbers), contains(2, 4));
   }
 
   @Test
@@ -27,5 +38,18 @@ public class HighOrderTest {
       }
     };
     assertThat(map(addOne, numbers), empty());
+  }
+
+  @Test
+  public void mapNonEmptyListAppliesFunctionToEachElement() {
+    Integer[] numbers = new Integer[] { 1, 2, 3, 4 };
+    // EXPLAIN: Assign a function to a variable
+    F<Integer, Integer> addOne = new F<Integer, Integer>() {
+      public Integer apply(Integer x) {
+        return x + 1;
+      }
+    };
+    // EXPLAIN: passing a function as an argument
+    assertThat(map(addOne, numbers), contains(2, 3, 4, 5));
   }
 }
