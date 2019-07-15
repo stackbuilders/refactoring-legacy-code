779db834b87d71cc5cfc49a7f22bd83e2908f603
Sebastián Estrella <sestrella.me@gmail.com>
2019-07-15 00:05:02 -0500

    Minor refactor

--- src/main/java/com/stackbuilders/javaday/BiFunction.java
+++ src/main/java/com/stackbuilders/javaday/BiFunction.java
@@ -4,10 +4,10 @@ public abstract class BiFunction<A, B, C> implements F<A, F<B, C>> {
   public F<B, C> apply(final A x) {
     return new F<B, C>() {
       public C apply(B y) {
-        return apply2(x, y);
+        return BiFunction.this.apply(x, y);
       }
     };
   }
 
-  public abstract C apply2(A x, B y);
+  public abstract C apply(A x, B y);
 }
