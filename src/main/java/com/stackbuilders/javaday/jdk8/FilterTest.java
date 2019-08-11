b09c4e16c68860df5db3c92784920e4003f4d261
Sebastián Estrella <sestrella.me@gmail.com>
2019-08-11 03:45:14 -0500

    Add isEven lambda version

--- src/main/java/com/stackbuilders/javaday/jdk8/FilterTest.java
+++ src/main/java/com/stackbuilders/javaday/jdk8/FilterTest.java
@@ -11,6 +11,12 @@ public class FilterTest {
 
   public static void test2() {
     Integer[] numbers = {1, 2, 3, 4, 5, 6};
+    Predicate<Integer> isEven = x -> x % 2 == 0;
+    Stream.of(numbers).filter(isEven);
+  }
+
+  public static void test3() {
+    Integer[] numbers = {1, 2, 3, 4, 5, 6};
     Predicate<Integer> isEven = new Predicate<Integer>() {
       public boolean test(Integer x) {
         return x % 2 == 0;
